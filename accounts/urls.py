3e05d0dfc82659218a7b223031c1aacc6fcc1251
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-06-09 21:42:13 +0000

    Base Navigation

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('link/generate/', views.generate_invite_link, name='generate_invite_link'),
     path('user/delete/<str:username>/', views.delete_user, name='delete_user' ),
     path('list/teachers/', views.TeacherListView.as_view(), name='teacher_list'),
-    path('list/parents/', views.ParentListView.as_view(), name='parents_list')
+    path('list/parents/', views.ParentListView.as_view(), name='parents_list'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from accounts.decorators import role_required
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your views here.
 
 
