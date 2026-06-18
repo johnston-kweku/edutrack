0c49b3c5423208c50c4d60c9791ed9373f2e561e
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-06-18 19:12:29 +0000

    Fix

--- academics/models.py
+++ academics/models.py
@@ -165,7 +165,7 @@ class Result(models.Model):
 
 
 class Attendance(models.Model):
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(default=timezone.now(), editable=False)
     class_marked = models.ForeignKey(Class, on_delete=models.PROTECT, null=True)
     marked_by = models.ForeignKey(User, on_delete=models.PROTECT, limit_choices_to={'role__in': ['TEACHING_STAFF', 'ADMIN']})
 
