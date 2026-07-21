385fc5ce25e4beb126ffb5182db1a71811c0e75d
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-07-21 18:49:08 +0000

    Save: progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -14,5 +15,33 @@ urlpatterns = [
     path('edit/profile/', views.edit_my_profile, name='edit_my_profile'),
     path('toggle/active/<int:user_id>/', views.toggle_active_state, name='toggle_active_state'),
     path('profile/', views.my_profile, name='my_profile'),
-    path('user/profile/<int:user_id>/',  views.user_profile, name='user_profile')
+    path('user/profile/<int:user_id>/',  views.user_profile, name='user_profile'),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='email_and_reset/password_reset.html',
+             email_template_name='email_and_reset/password_reset_email.html',
+             subject_template_name='email_and_reset/password_reset_subject.txt',
+             success_url='/password-reset/done/'
+         ),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='email_and_reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='email_and_reset/password_reset_confirm.html',
+             success_url='/reset/done/'
+         ),
+         name='password_reset_confirm'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='email_and_reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
