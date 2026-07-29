355b7c3b6891de0c916a4d83d81dc503d63efa50
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-07-29 15:47:26 +0000

    refactor: deactivation logic to use ajax

--- static/js/accounts/changeActiveState.js
+++ static/js/accounts/changeActiveState.js
@@ -1,5 +1,7 @@
 const activeToggleForm = document.querySelector('.active-toggle');
 const activeBtn = document.querySelector('.active-btn');
+const activeBtnText = activeBtn.querySelector('span');
+const activeStatus = document.querySelector('.active-status');
 const userId = activeToggleForm.dataset.userId;
 
 activeToggleForm.addEventListener('submit', (e) => {
@@ -22,25 +24,37 @@ async function toggleState(userId) {
     if (data.success) {
         toast.show(data.message, 'success')
         if (data.is_active) {
-            activeBtn.classList.remove(
+            activeBtn.classList.add(
                 'text-red-700', 'bg-red-50', 'border-red-200',
                 'hover:bg-red-100'
             )
-            activeBtn.classList.add(
+            activeBtn.classList.remove(
                 'text-green-700', 'bg-green-50', 'border-green-200',
                 'hover:bg-green-100'
             )
-            activeBtn.textContent = data.action
+            activeBtnText.textContent = data.action
+            activeStatus.textContent = data.status
+            activeStatus.classList.remove(
+                'text-red-700', 'bg-red-100',
+                'border-red-400'
+            )
+            activeStatus.classList.add('bg-green-100', 'text-green-700', 'border-red-400')
         }else {
-            activeBtn.classList.remove(
+            activeBtn.classList.add(
                 'text-green-700', 'bg-green-50', 'border-green-200',
                 'hover:bg-green-100'
             )
-            activeBtn.classList.add(
+            activeBtn.classList.remove(
                 'text-red-700', 'bg-red-50', 'border-red-200',
                 'hover:bg-red-100'
             )
-            activeBtn.textContent = data.action
+            activeBtnText.textContent = data.action
+            activeStatus.textContent = data.status
+            activeStatus.classList.remove('bg-green-100', 'text-green-700', 'border-red-400')
+            activeStatus.classList.add(
+                'text-red-700', 'bg-red-100',
+                'border-red-400'
+            )
         }
     }else {
         toast.show(data.message)
